cdb74c380e7bf4dc6bf6a78b7a00ce2a475b33c9
Hack Reactor Students <noahjcz@gmail.com>
2013-11-18 21:05:30 -0800

    added stuff

---
+++ spec/lineUpSpec.js
@@ -0,0 +1,28 @@
+describe("lineUpDancer", function(){
+
+  var dancer1;
+  var dancer2;
+
+  beforeEach(function(){
+    dancer1 = new robotDancer(5, 5, 5000);
+    dancer2 = new blinkyDancer(42, 42, 4242);
+  });
+
+  it("should have a lineUp function", function(){
+    expect(dancer.lineUp).to.be.an.instanceof(Function);
+  });
+
+  it("should change their css", function(){
+    var previousCSS = dancer1.$node.css();
+    dancer1.lineUp();
+    expect(dancer1.$node.css() === previousCSS).to.be.false;
+  });
+
+  it("should iterate over all robots", function(){
+    var dancer1CSS = dancer1.$(node).css();
+    var dancer2CSS = dancer2.$(node).css();
+    $('.lineUpButton').click();
+    expect(dancer1.$node.css() === dancer1CSS).to.be.false;
+    expect(dancer2.$node.css() === dancer2CSS).to.be.false;
+  });
+});
---
+++ src/robotDancer.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+var RobotDancer = function(top, left, timeBetweenSteps){
+  Dancer.apply(this,arguments);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+  this.$node = $('<span class = "robotDancer"> <img src = "robot1.jpg"/></span>');
+  this.setPosition(top, left);
+};
+
+RobotDancer.prototype = Object.create(Dancer.prototype);
+RobotDancer.prototype.constructor = RobotDancer;
+RobotDancer.prototype.step = function(timeBetweenSteps){
+  Dancer.prototype.step.call(this,timeBetweenSteps);
+  var danceMode = Math.ceil(Math.random()*3);
+  var danceMode2 = Math.ceil(Math.random()*5);
+    this.$node.animate({top : danceMode*200, left: danceMode2*150, rotation: danceMode2 + "deg"});
+  this.$node.html('<span class = "robotDancer"> <img src = "robot' + danceMode + '.jpg"/></span>');
+  
+};
